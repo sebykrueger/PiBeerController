59c2f710dd7dc59ab29ffe73081a556f0d52fbb5
sebykrueger <sebykrueger@gmail.com>
2013-10-30 00:38:12 +1300

    add maven pom.xml

---
+++ deployToPi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp /Users/sebastiankrueger/.m2/repository/BeerController/BeerController/0.0.1-SNAPSHOT/BeerController-0.0.1-SNAPSHOT.jar pi@192.168.1.58:java/apps
+#scp /Users/sebastiankrueger/.m2/repository/org/restlet/jse/org.restlet/2.1.4/org.restlet-2.1.4.jar pi@192.168.1.58:java/apps
+
